182ba48204417bca5de942226994216058df967c
Alex Roden <alex.broom-roden@oneiota.co.uk>
2023-07-17 09:07:36 +0100

    fix merge conflict

--- pkg/aws/response.go
+++ pkg/aws/response.go
@@ -5,8 +5,25 @@ import (
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/itsoneiota/lambda-handlers/pkg/handler"
 )
+
+type LambdaCallback = func(request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error)
+
+func Start(h handler.HandlerFunc) {
+	lambda.Start(
+		getHandler(h),
+	)
+}
+
+func getHandler(h handler.HandlerFunc) LambdaCallback {
+	return func(r *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+		res, err := h(Context{r.RequestContext}, NewAWSRequest(r))
+
+		return NewEvent(res), err
+	}
+}
 
 func NewEvent(r *handler.Response) *events.APIGatewayProxyResponse {
 	return &events.APIGatewayProxyResponse{
