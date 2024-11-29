9dcdb6a6fcff24653e8ad5366b66f5286f3f8ee8
Alex Roden <alex.broom-roden@oneiota.co.uk>
2024-11-29 13:16:45 +0000

    optimise middleware

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -3,8 +3,8 @@ package handler
 type Middleware func(HandlerFunc) HandlerFunc
 
 func (rh *Handler) Middlewares(h HandlerFunc, middlewares ...Middleware) HandlerFunc {
-	for i := len(middlewares) - 1; i >= 0; i-- {
-		h = middlewares[i](h)
+	for _, middleware := range middlewares {
+		h = middleware(h)
 	}
 
 	return h
